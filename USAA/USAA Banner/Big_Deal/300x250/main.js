384ed83f700ed2f8fe659b0a827ce039e104a93f
MarinoSy <symarino@gmail.com>
2023-04-25 10:51:51 -0500

    Initial upload

---
+++ USAA/USAA Banner/Big_Deal/300x250/main.js
@@ -0,0 +1,9 @@
+this.addEventListener('DOMContentLoaded', loadCSS);
+
+function loadCSS(){
+    var css = document.createElement( 'link' );
+    css.setAttribute( 'rel', 'stylesheet' );
+    css.setAttribute( 'type', 'text/css' );
+    css.setAttribute( 'href', "style.css" );
+    document.getElementsByTagName('head')[0].appendChild(css);
+}
